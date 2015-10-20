6c5aec70cc277a113be63246da3915e2e03b26c8
Wilson Carlos <Wilson Carlos>
2015-10-20 16:12:09 -0200

    'PrimeitoCommit'

---
+++ src/main/java/br/com/direcaocerta/App.java
@@ -0,0 +1,13 @@
+package br.com.direcaocerta;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
