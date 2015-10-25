d6e09235d3dcf0e7e930bc8a5003de846d73e6f9
Wilson Carlos <Wilson Carlos>
2015-10-25 12:14:23 -0200

    Iniciando Servlet

---
+++ src/main/java/br/com/direcaocerta/controller/AdministradorController.java
@@ -0,0 +1,27 @@
+package br.com.direcaocerta.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name="/administradorController.do")
+
+public class AdministradorController extends HttpServlet{
+
+	public AdministradorController() {
+	System.out.println("Novo Servlet");
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Chamou!");
+		super.doGet(req, resp);
+	}
+}
